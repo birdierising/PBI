08ea92fdde6923d79d28ccd0608c2d39a06645b6
Alexandria Ostrem <alex.ostrem@gmail.com>
2021-08-27 12:08:39 -0700

    add/update/delete plants

--- planterbase_interface.py
+++ planterbase_interface.py
@@ -8,6 +8,8 @@ indoorconnection = msc.connect(option_files='/etc/mysql/conf.d/pbcli.cnf')
 
 indoorcursor = indoorconnection.cursor()
 
+# read section
+
 def defaultsearch ( defaulttosearch ):
     defaulttosearch = sys.argv[1]
 
@@ -142,6 +144,71 @@ def vendorsearch ( vendortosearch ):
     else:
         pprint.pprint(data)
 
+# writes section
+
+def addplant ( planttoadd ):
+    planttoadd = sys.argv[2]
+    pid = int(input("Enter plant id: "))
+    q = int(input("How many? "))
+    ln = input("Enter Latin binomial: ")
+    pg = input("Enter plant group: ")
+    v = input("What vendor did this plant come from? ")
+    s = input("What are its sun requirements? ")
+    w = input("What are its watering needs? ")
+    f = input("Does it flower? Y or N only ")
+    l = input("Where does it live? ")
+    n = input("Any additional notes? ")
+    addquery = f"INSERT INTO plants (plantid, common_name, quantity, latin_name, plant_group, vendor, sunlight, water, flowering, location, notes) VALUES ({pid}, '{planttoadd}', '{q}', '{ln}', '{pg}', '{v}', '{s}', '{w}', '{f}', '{l}', '{n}');"
+    indoorcursor.execute(addquery)
+    indoorconnection.commit()
+    checksuccess = f"SELECT * from plants WHERE common_name='{planttoadd}';"
+    indoorcursor.execute(checksuccess)
+    data = indoorcursor.fetchall()
+    if not data:
+        print("Something went wrong here")
+    else:
+        pprint.pprint(data)
+    indoorcursor.close()
+    indoorconnection.close()
+    exit
+
+def deleteplant ( planttodelete ):
+    planttodelete = sys.argv[2]
+
+    deletequery = f"DELETE FROM plants WHERE common_name='{planttodelete}';"
+    indoorcursor.execute(deletequery)
+    indoorconnection.commit()
+
+def updateplantnote ( updateplantnote ):
+    planttoupdate = sys.argv[2]
+
+    notes = input("What notes need updated? ")
+    updatequery = f"UPDATE plants SET notes='{notes}' WHERE common_name='{planttoupdate}';"
+    indoorcursor.execute(updatequery)
+    indoorconnection.commit()
+    checksuccess = f"SELECT common_name,notes FROM plants WHERE common_name='{planttoupdate}';"
+    indoorcursor.execute(checksuccess)
+    data = indoorcursor.fetchall()
+    if not data:
+        print("Something went wrong here")
+    else:
+        pprint.pprint(data)
+
+def updateplantquantity ( updateplantquantity ):
+    planttoupdate = sys.argv[2]
+
+    quantity = input("What plant quantity need updated? ")
+    updatequery = f"UPDATE plants SET quantity='{quantity}' WHERE common_name='{planttoupdate}';"
+    indoorcursor.execute(updatequery)
+    indoorconnection.commit()
+    checksuccess = f"SELECT common_name,quantity FROM plants WHERE common_name='{planttoupdate}';"
+    indoorcursor.execute(checksuccess)
+    data = indoorcursor.fetchall()
+    if not data:
+        print("Something went wrong here")
+    else:
+        pprint.pprint(data)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("default",nargs='?',type=defaultsearch)
 parser.add_argument('-b','--binomial','--latinname', type=latinnamesearch,action='store')
@@ -155,5 +222,9 @@ parser.add_argument('-l', '--location', type=locationsearch,action='store')
 parser.add_argument('-n', '--notes', type=notessearch,action='store')
 parser.add_argument('-s', '--sunreqs', type=sunlightsearch,action='store')
 parser.add_argument('-v', '--vendor', type=vendorsearch,action='store')
+parser.add_argument('-a', '--add', type=addplant,action='store')
+parser.add_argument('-u', '--updatenote', type=updateplantnote,action='store')
+parser.add_argument('-q', '--updatequantity', type=updateplantquantity,action='store')
+parser.add_argument('-z', '--delete', type=deleteplant,action='store')
 
 args = parser.parse_args()
