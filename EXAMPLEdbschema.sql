a824f8d5ceeb5e2aad120dcac5999847aff1118e
Alexandria Ostrem <alex.ostrem@gmail.com>
2021-09-21 10:42:50 -0700

    NOT NULL and changing date to bigint

--- EXAMPLEdbschema.sql
+++ EXAMPLEdbschema.sql
@@ -4,16 +4,17 @@ USE indoorplants;
 
 CREATE TABLE `plants` (
   `plantid` mediumint(25) NOT NULL AUTO_INCREMENT,
-  `common_name` varchar(100) DEFAULT NULL,
-  `latin_name` varchar(100) DEFAULT NULL,
-  `plant_group` varchar(100) DEFAULT NULL,
-  `date_acquired` date DEFAULT NULL,
-  `vendor` varchar(100) DEFAULT NULL,
-  `sunlight` varchar(50) DEFAULT NULL,
-  `water` varchar(50) DEFAULT NULL,
-  `flowering` char(3) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
-  `notes` varchar(255) DEFAULT NULL,
+  `common_name` varchar(100) NOT NULL,
+  `quantity` tinyint(50) DEFAULT 1,
+  `latin_name` varchar(100) NOT NULL,
+  `plant_group` varchar(100) NOT NULL,
+  `date_acquired` bigint(11) NOT NULL,
+  `vendor` varchar(100) NOT NULL,
+  `sunlight` varchar(50) NOT NULL,
+  `water` varchar(50) NOT NULL,
+  `flowering` char(3) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `notes` varchar(255) NOT NULL,
   PRIMARY KEY (`plantid`),
   UNIQUE KEY `common_name` (`common_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb;
